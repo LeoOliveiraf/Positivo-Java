2050ebd36d9ec449e9182f62829ea1f80862d64d
Leonardo de Oliveira Florindo <oliveiraleo797@gmail.com>
2023-03-10 16:34:52 -0300

    Treinos do dia 10/03/2023

--- src/Exemplo04/Exemplo03.java
+++ src/Exemplo04/Exemplo03.java
@@ -9,13 +9,18 @@ public class Exemplo03 {
 		String sobrenome = input.next();
 		String nomeCompleto = null;
 		int contador = 0;
+		String total = " ";
 		System.out.println("Digite os Nomes Completos: ");
 		do {
 			nomeCompleto = input.nextLine();
 		if(nomeCompleto.contains(sobrenome)) {
 			contador++;
+			total = total.concat(" ");
+			total = nomeCompleto.concat(total);
+			total = total.concat(" ");
 			}	
 		}while(!nomeCompleto.equals("Fim"));
 		System.out.println(contador);
+		System.out.println(total);
 	}
 }
